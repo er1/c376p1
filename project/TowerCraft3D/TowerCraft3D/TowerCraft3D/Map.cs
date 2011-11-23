3c0627a3ef316dddc255ae7f466ab995108016a8
Sevan Gurunlu <alphagman@gmail.com>
2011-11-23 17:40:55 -0500

    Added map, tile, tower stuff

---
+++ project/TowerCraft3D/TowerCraft3D/TowerCraft3D/Map.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TowerCraft3D
+{
+    public struct TileCoord
+    {
+        int x;
+        int y;
+    }
+
+    class Map
+    {
+        private Dictionary<TileCoord, Tile> map { get; set; }
+
+        private Map()
+        {
+            map = new Dictionary<TileCoord, Tile>();
+        }
+
+        public Tile GetTile(TileCoord tc)
+        {
+            return map[tc];
+        }
+
+        public void SetTile(TileCoord tc, Tile tile)
+        {
+            map[tc] = tile;
+        }
+
+        public bool doesTileExist(TileCoord tc)
+        {
+            if (map.ContainsKey(tc))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ project/TowerCraft3D/TowerCraft3D/TowerCraft3D/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerCraft3D
+{
+    class Tile
+    {
+        HashSet<model> entities;
+
+        public Tile()
+        {
+        }
+
+
+
+    }
+}
