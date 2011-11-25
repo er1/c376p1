0a64a9bc5875bc7e65ab9f1930c2fc691c8db214
Kyle <kembs@example.com>
2011-11-24 21:07:05 -0500

    Fixed Cam pos

--- project/TowerCraft3D/TowerCraft3D/TowerCraft3D/Game1.cs
+++ project/TowerCraft3D/TowerCraft3D/TowerCraft3D/Game1.cs
@@ -56,7 +56,7 @@ namespace TowerCraft3D
             //Camera component
             Components.Remove(cameraMain);
             Components.Remove(modelManager);
-            cameraMain = new Camera(this, new Vector3(0, 0, 1), Vector3.Zero, Vector3.Up, MainScreen, true,worldSize);
+            cameraMain = new Camera(this, new Vector3(0, 125, 125), new Vector3(0,-5,1), Vector3.Up, MainScreen, true,worldSize);
             Components.Add(cameraMain);
             
             this.IsFixedTimeStep = false;
