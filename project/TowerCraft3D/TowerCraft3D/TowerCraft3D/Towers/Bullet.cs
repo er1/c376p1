37ec1939b964c53c284fd2d5da86c5c26c6bee16
Sevan Gurunlu <alphagman@gmail.com>
2011-11-29 14:56:17 -0500

    Inserted bullets.

---
+++ project/TowerCraft3D/TowerCraft3D/TowerCraft3D/Towers/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TowerCraft3D
+{
+    class Bullet : projectile
+    {
+        public Bullet(ref Model temp, ref Model colModel, Vector3 location, Vector3 newDirection)
+            : base(ref temp, ref colModel, location, newDirection)
+        {
+            world = Matrix.CreateRotationZ((float)Math.PI/2) * Matrix.CreateScale(1) * Matrix.CreateTranslation(location);
+        }
+    }
+}
