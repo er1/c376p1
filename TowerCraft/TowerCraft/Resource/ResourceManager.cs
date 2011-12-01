b63ec142e4bb0a854857758f705402288bc316d6
Kyle <kembs@example.com>
2011-12-01 14:42:02 -0500

    derp

--- TowerCraft/TowerCraft/Resource/ResourceManager.cs
+++ TowerCraft/TowerCraft/Resource/ResourceManager.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;s
+using Microsoft.Xna.Framework.Media;
 using TowerCraft3D;
 
 namespace TowerCraft
